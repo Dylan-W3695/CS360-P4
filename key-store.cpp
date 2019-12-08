2f6e64b87f8870bd929ca998ed8d3d0756491422
Wahl_D <wahl_d@CAKE.lynchburg.edu>
2019-12-08 16:47:57 -0500

    started to work with getopt

--- key-store.cpp
+++ key-store.cpp
@@ -4,24 +4,22 @@
 #include <stdio.h>
 #include <queue>
 #include <list>
+#include <unistd.h>
+#include <getopt.h>
 
 using namespace std;
 
+void handleArgs(int argc, char** argv);
+
 int main(int argc, char** argv){
-	string action = NULL;
-	string data = NULL;
+    string action = NULL;
+    string data = NULL;
 	
-    if (argc >= 3){
-	string data = argv[2];
-        if(data == "--file" && argc == 4){
-            data = argv[3];
-        }
-    }else if(argc == 1){
-	cerr << "Must pass arguments in: pass '--help' to see signature" << endl;
-	exit(1);
-    }
 
 
+    cout << getopt_long(argc, argv, NULL, long_opts, NULL) << endl;
+	
+    /*
 
     if(action == "--store" && data != NULL){
 	
@@ -36,6 +34,24 @@ int main(int argc, char** argv){
 	cerr << "Please enter a valid argument: pass '--help' to see signature." << endl;
 	exit(1);
     }
+*/
 
     return 0;
+}
+
+
+void handleArgs(int argc, char** argv){
+
+    const char* optString = "hs:r:";
+    const option fullOpts[] = {
+	{"store", required_argument, NULL, 'n'},
+	{"retrieve", required_argument, NULL, 'r'},
+	{"help", no_argument, NULL, h}
+    };
+
+    int opt = getopt_long(argc, argv, optString, fullOpts, NULL);
+	
+    if(opt == -1){
+    cerr << "getopt failed";
+    exit(1);
 }
