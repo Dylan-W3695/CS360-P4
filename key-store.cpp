c0c8f7045c15f9c6cef0776a8dcd7f337c67258f
WELSH_BD <welsh_bd@PIE.lynchburg.edu>
2019-12-10 13:58:37 -0500

    Fixed getopt_long implementation. Detects all args passed in. Still need to work on what to do once options are detected.

--- key-store.cpp
+++ key-store.cpp
@@ -11,47 +11,59 @@ using namespace std;
 
 void handleArgs(int argc, char** argv);
 
-int main(int argc, char** argv){
-    string action = NULL;
-    string data = NULL;
-	
-
-
-    cout << getopt_long(argc, argv, NULL, long_opts, NULL) << endl;
-	
-    /*
-
-    if(action == "--store" && data != NULL){
-	
-
-    }else if(action == "--retrieve" && data != NULL){
-	
-
-    }else if(action == "--help"){
-	cout << "key-store <--store key> <value | --file file_path> | <--retrieve key> [--file file_path] | [--help]" << endl; 
-
-    }else{
-	cerr << "Please enter a valid argument: pass '--help' to see signature." << endl;
-	exit(1);
-    }
-*/
-
-    return 0;
+int main(int argc, char** argv)
+{
+  handleArgs(argc,argv);
+  
+  exit(EXIT_SUCCESS);
 }
 
 
 void handleArgs(int argc, char** argv){
 
-    const char* optString = "hs:r:";
-    const option fullOpts[] = {
-	{"store", required_argument, NULL, 'n'},
-	{"retrieve", required_argument, NULL, 'r'},
-	{"help", no_argument, NULL, h}
+  //store, retrieve, and file all require arguments, so are followed by a colon in optString. Help does not require one.
+  const char* const optString = "s:r:f:h";
+  const option fullOpts[] =
+    {
+      {"store", required_argument, NULL, 's'},
+      {"retrieve", required_argument, NULL, 'r'},
+      {"file", required_argument, NULL, 'f'},
+      {"help", no_argument, NULL, 'h'}
     };
 
-    int opt = getopt_long(argc, argv, optString, fullOpts, NULL);
-	
-    if(opt == -1){
-    cerr << "getopt failed";
-    exit(1);
+  while(true)
+    {
+      const int opt = getopt_long(argc, argv, optString, fullOpts, NULL);
+      
+      if(opt == -1)
+	{
+	  cerr << "getopt failed";
+	  exit(EXIT_FAILURE);
+	}
+
+      switch(opt)
+	{
+	case 'h':
+	  cout << endl <<"key-store <--store key> | <--retrieve key> <value | --file file_path> [--help]" << endl;
+	  exit(EXIT_SUCCESS);
+	  
+	case 's':
+	  cout <<"Got command to STORE: " <<  optarg << endl;
+	  break;
+
+	case 'r':
+	  cout << "Got command to RETRIEVE: " << optarg << endl;
+	  break;
+
+	case 'f':
+	  cout << "Got FILE arg: " << optarg << endl;
+	  break;
+	  
+	default:
+	  // If they didn't add any args, print help signature and exit
+	  cout << endl <<"key-store <--store key> | <--retrieve key> <value | --file file_path> [--help]" << endl;
+	  exit(EXIT_SUCCESS);
+	}
+    }
 }
+  
